485dbe25792160a14de4d7d562ed2f681fc79ba2
Ivan Contreras <gcontreras31996@gmail.com>
2023-07-24 17:37:46 -0500

    added a few files

---
+++ modules.js
@@ -0,0 +1,8 @@
+// Modules - Encapsulated Code (only share minimum)
+// CommonJS - every file is module (by default)
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`)
+}
+
+sayHi('Jerry')
