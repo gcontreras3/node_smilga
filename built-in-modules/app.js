ba7e01d5bf27dfadbfaba5fcb5bc0f33942e3b77
Ivan Contreras <gcontreras31996@gmail.com>
2023-07-25 11:56:11 -0500

    added packages

--- built-in-modules/app.js
+++
@@ -1,8 +0,0 @@
-const {readFileSync, writeFileSync, readFile} = require('fs');
-
-
-const first = readFileSync('./built-in-modules/content/first.txt', 'utf-8')
-const second = readFileSync('./built-in-modules/content/second.txt', 'utf-8')
-
-
-writeFileSync('./built-in-modules/content/result-sync.txt', `Here is the result ${first}, ${second}`)
